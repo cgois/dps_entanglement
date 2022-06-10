09b2155e6f1f8c7da6e9f182f27d0ce9570751c8
Carlos de Gois <carlosbgois@gmail.com>
2022-06-11 01:36:53 +0200

    Working but unverified.

--- BosonicSymmetry.jl
+++ BosonicSymmetry.jl
@@ -12,32 +12,6 @@ function symmetric_projection(dim::Int, copies::Int)
     orthonormal_range(P / length(perm_list))
 end
 
-"""Threaded version of `symmetric_projection`."""
-function par_symmetric_projection(dim::Int, copies::Int) 
-    # The symmetric projection is the average over all permutation operators.
-    perm_list = collect(permutations(1:copies))
-    P = zeros(dim^copies, dim^copies, length(perm_list))
-    Threads.@threads for i in 1:length(perm_list)
-        P[:,:,i] = permutation_operator(dim, perm_list[i])
-    end
-    P = sum(P, dims=3)[:,:,1]
-    # Inverse of projector onto the symmetric subspace.
-    orthonormal_range(P / length(perm_list))
-end
-
-"""Threaded version of `symmetric_projection`."""
-function par_symmetric_projection(dim::Int, copies::Int) 
-    # The symmetric projection is the average over all permutation operators.
-    perm_list = collect(permutations(1:copies))
-    P = zeros(dim^copies, dim^copies, length(perm_list))
-    Threads.@threads for i in 1:length(perm_list)
-        P[:,:,i] = permutation_operator(dim, perm_list[i])
-    end
-    P = sum(P, dims=3)[:,:,1]
-    # Inverse of projector onto the symmetric subspace.
-    orthonormal_range(P / length(perm_list))
-end
-
 """Unitary to permute subsystems of dimension `dim` w.r.t. vector `perm`."""
 function permutation_operator(dim::Int, perm::Vector)
     # This trick only works up to a 62x62 permutation matrix because of basis conversion.
@@ -60,9 +34,4 @@ end
 """Put numbers smaller than `ZERO_TOL` to 0."""
 function chopzeros(it; ZERO_TOL=1E-11)
     map(x -> abs(x) < ZERO_TOL ? 0.0 : x, it)
-end
-
-"""Check if input is a projector onto the symmetric subspace."""
-function check_symm_projection(P)
-    # TODO
 end
